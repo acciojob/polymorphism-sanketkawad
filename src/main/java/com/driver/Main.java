db4ba486488055ec263d6698ccd0e50bae3fed0c
sanket kawad <sanket kawad@RadZEN.(none)>
2023-01-22 16:37:13 +0530

    solution 2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,13 +3,10 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
         Product p = new Product();
-        System.out.println(p.product(2,3));
-        System.out.println(p.product(2,3,4));
-        System.out.println(p.product(2.0,3.0));
 
-
-
-
+        p.product(2,3);
+        p.product(2,3,4);
+        p.product(2.0,3.0);
     }
 }
 
